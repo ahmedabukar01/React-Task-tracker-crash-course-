3cacd3297e2972648612786a9bcae91996c5d914
ahmed <ahmedlearn01@gmail.com>
2021-09-02 08:52:51 -0700

    initial commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,32 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-// const Header = ()=>{
-//     return(
-//         <div>
-//             <h1>hey its me man!</h1>
-//         </div>
-//     )
-// }
+const Header = ({name,title})=>{
+    return(
+        <div>
+            <h1 style={myStyle}>hey its me man! {name}{title}</h1>
+        </div>
+    )
+}
 
-class Header extends React.Component{
-    render(){
-      return(
-        <h2>hey you im still working in the old school :) ya you got something</h2>
-      ) 
-    }
-  }
+Header.defaultProps = {
+    title: 'the comeback history',
+}
+Header.propTypes = {
+    title: PropTypes.string.isRequired
+}
+
+        // CSS IN JS
+        const myStyle = {
+            color: 'red',
+            background: 'blue',
+        }
+        // class
+// class Header extends React.Component{
+//     render(){
+//       return(
+//         <h2>hey you im still working in the old school :) ya you got something</h2>
+//       ) 
+//     }
+//   }
 export default Header
