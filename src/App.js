9793cf1679e206e57d6fcac982bcdf27bb0ed0d2
ahmed <ahmedlearn01@gmail.com>
2021-09-21 17:08:05 -0700

    intial commit

--- src/App.js
+++ src/App.js
@@ -7,36 +7,7 @@ import AddTask from './components/AddTask'
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
   const [tasks,setTasks] = useState([
-    {
-        id: 1,
-        text: 'playing football',
-        day:'feb 5th at 1:30',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'cleaning house',
-        day:'feb 5th at 8:15',
-        reminder: false,
-    },
-    {
-        id: 3,
-        text: 'food shopping',
-        day:'feb 5th at 10:00',
-        reminder: true,
-    },
-    {
-        id: 4,
-        text: 'meeting at school',
-        day:'feb 5th at 2:30',
-        reminder: false,
-    },
-    {
-        id: 5,
-        text: 'shooting flim',
-        day:'feb 5th at 5:50',
-        reminder: true,
-    }
+    
 ])
 
 // delete task
